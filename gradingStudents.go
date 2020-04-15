642c81dad46280d76fe700f644cc78f1d01f634c
esteban0477 <esteban_0203@hotmail.com>
2020-04-14 20:50:29 -0500

    Create gradingStudents.go

---
+++ gradingStudents.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+func gradingStudents(grades []int32) []int32 {
+    // Write your code here
+    gradesRounded := make([]int32, len(grades))
+    //fmt.Println(grades)
+
+    var next int32 = 0
+
+    for i := 0; i < len(grades); i++ {
+        if grades[i] < 38 || grades[i]%5 == 0 {
+            gradesRounded[i] = grades[i]
+        } else {
+            next = grades[i] + (5 - (grades[i] % 5))
+            if next-grades[i] < 3 {
+                gradesRounded[i] = next
+            } else {
+                gradesRounded[i] = grades[i]
+            }
+        }
+    }
+    return gradesRounded
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    gradesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    var grades []int32
+
+    for i := 0; i < int(gradesCount); i++ {
+        gradesItemTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+        checkError(err)
+        gradesItem := int32(gradesItemTemp)
+        grades = append(grades, gradesItem)
+    }
+
+    result := gradingStudents(grades)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, "\n")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
