be9d8116d1a380658469338fee61bf942314950c
esteban0477 <esteban_0203@hotmail.com>
2020-05-07 19:15:12 -0500

    Create interfacesWithAssertion.go

---
+++ interfacesWithAssertion.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last, " - the secretAgent speak")
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, p.last, " - the person speak")
+}
+
+type human interface {
+	speak()
+}
+
+// The next function is using assertion: h.(person).first
+func bar(h human) {
+	switch h.(type) {
+	case person:
+		fmt.Println("I was passed into barrrrrr", h.(person).first)
+	case secretAgent:
+		fmt.Println("I was passed into barrrrrr", h.(secretAgent).first)
+	}
+	fmt.Println("I was passed into bar", h)
+}
+
+func main() {
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
+
+	p1 := person{
+		first: "Dr.",
+		last:  "Yes",
+	}
+
+	bar(sa1)
+	bar(sa2)
+	bar(p1)
+}
